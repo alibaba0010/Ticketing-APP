64e4613d3ddbd8845397806987d77ea1940698ff
alibaba0010 <zakariyahali100@gmail.com>
2023-07-16 19:11:57 +0100

    solved kubernetes issue

--- auth/src/server.ts
+++ auth/src/server.ts
@@ -1,8 +1,7 @@
 import express, { json } from "express";
-import userRouter from "./routes/user.router";
+// import userRouter from "./routes/user.router";
 const app = express();
-app
-  .use(json())
-  .use("api/v1/users", userRouter);
-
+app.use(json());
+// .use("api/v1/users", userRouter);
+console.log("hello!");
 app.listen(3001, () => console.log("Listen to port 3001"));
