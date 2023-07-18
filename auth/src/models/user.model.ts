8e28568bcd5d36c2220c0ff1150548f3d8608827
alibaba0010 <zakariyahali100@gmail.com>
2023-07-18 23:34:22 +0100

    Working with mongoose model

---
+++ auth/src/models/user.model.ts
@@ -0,0 +1,34 @@
+import pkg from "mongoose";
+const { Schema, model, Model, Document } = pkg;
+
+// An interface that describes the properties
+// that are requried to create a new User
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties
+// that a User Model has
+interface UserModel extends Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties
+// that a User Document has
+interface UserDoc extends Document {
+  email: string;
+  password: string;
+}
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export default model<UserDoc, UserModel>("User", UserSchema);
