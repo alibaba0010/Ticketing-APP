c8e0c1ff0a613beb9ef02416a53d113778884cf0
alibaba0010 <zakariyahali100@gmail.com>
2023-08-14 16:16:19 +0100

    debugging ingress

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 export default ({ req }) => {
   if (typeof window === "undefined") {
+    console.log("Req: ", req.headers);
     // We are on the server
 
     return axios.create({
       baseURL:
-        // "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
-      "http://ingress-nginx-controller.ingress-nginx",
+        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
 
-      // headers: req.headers,
-      headers: {
-        Host: "http://192.168.49.2:32208",
-      },
+      headers: req.headers,
+      // headers: {
+      //   Host: "http://192.168.49.2:32208",
+      // },
     });
   } else {
     // We must be on the browser
