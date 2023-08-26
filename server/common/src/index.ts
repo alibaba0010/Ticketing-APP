0de8ccd64eabc1419ae1e2767c512d337b2857a0
alibaba0010 <zakariyahali100@gmail.com>
2023-08-26 15:20:20 +0100

    Updates

--- server/common/src/index.ts
+++ server/common/src/index.ts
@@ -5,7 +5,6 @@ export { default as NotFoundError } from "./errors/notFoundError";
 export { default as RequestValidationError } from "./errors/validationError";
 export { default as DatabaseConnectionError } from "./errors/dbConnectionError";
 
-
 export * from "./errors/errorHandler";
 
 export * from "./middlewares/currentUser";
@@ -17,3 +16,5 @@ export * from "./event-handlers/base-publisher";
 export * from "./event-handlers/subjects";
 export * from "./event-handlers/events/ticket-created-event";
 export * from "./event-handlers/events/ticket-updated-event";
+
+export * from "./event-handlers/types/order-status";
