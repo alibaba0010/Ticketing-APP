b53bce94cdfda1266711cdf3f17dccd9363e25d8
alibaba0010 <zakariyahali100@gmail.com>
2023-08-30 20:39:17 +0100

    debugging

--- server/orders/src/evemts-handler/listeners/ticket-created-listener.ts
+++ server/orders/src/evemts-handler/listeners/ticket-created-listener.ts
@@ -1,12 +1,17 @@
-import { Message } from 'node-nats-streaming';
-import { Subjects, Listener, TicketCreatedEvent,  queueGroupNameOrders  } from '@alibabatickets/common';
-import { Ticket } from 'orders/src/models/tickets-orders';
+import { Message } from "node-nats-streaming";
+import {
+  Subjects,
+  Listener,
+  TicketCreatedEvent,
+  queueGroupNameOrders,
+} from "@alibabatickets/common";
+import { Ticket } from "../../models/tickets-orders";
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = queueGroupNameOrders;
 
-  async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+  async onMessage(data: TicketCreatedEvent["data"], msg: Message) {
     const { id, title, price } = data;
 
     const ticket = Ticket.build({
