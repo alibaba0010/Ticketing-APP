1fafdfe369a6a778e05ff37ee37e338153a1b28b
alibaba0010 <zakariyahali100@gmail.com>
2023-09-02 08:12:08 +0100

    working client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,47 +1,45 @@
 import Link from "next/link";
 import client from "../api/build-client";
-
+// tickets & current user from server
 const LandingPage = ({ currentUser, tickets }) => {
-  //   const ticketList = tickets.map((ticket) => {
-  //     return (
-  //       <tr key={ticket.id}>
-  //         <td>{ticket.title}</td>
-  //         <td>{ticket.price}</td>
-  //         <td>
-  //           <Link
-  //             href="/tickets/[ticketId]"
-  //             as={`/tickets/${ticket.id}`}
-  //             legacyBehavior
-  //           >
-  //             <a>View</a>
-  //           </Link>
-  //         </td>
-  //       </tr>
-  //     );
-  //   });
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+        <td>
+          <Link
+            href="/tickets/[ticketId]"
+            as={`/tickets/${ticket.id}`}
+            legacyBehavior
+          >
+            <a>View</a>
+          </Link>
+        </td>
+      </tr>
+    );
+  });
 
-  //   return (
-  //     <div>
-  //       <h2>Tickets</h2>
-  //       <table className="table">
-  //         <thead>
-  //           <tr>
-  //             <th>Title</th>
-  //             <th>Price</th>
-  //             <th>Link</th>
-  //           </tr>
-  //         </thead>
-  //         <tbody>{ticketList}</tbody>
-  //       </table>
-  //     </div>
-  //   );
-  // };
-  console.log("In Tickets");
+  return (
+    <div>
+      <h2>Tickets</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
+  );
 };
-// server fetch data SSR-- server side rendering process
+
+// server fetch data SSR-- server side rendering process --- fetching from server
 LandingPage.getInitialProps = async (context, client, currentUser) => {
-  // const { data } = await client.get("/api/tickets");
-  const { data } = await client.get("/api/v1/users/currentuser");
+  const { data } = await client.get("/api/v1/tickets");
 
   return { users: data };
 };
--- server/orders/src/routes/orders.router.ts
+++ server/orders/src/routes/orders.router.ts
@@ -17,6 +17,5 @@ ticketRouter
   .get("/:id", authentication, getOrderWithId)
   .get("/", authentication, getOrders)
   .post("/", authentication, validateOrder, validateRequest, createOrder)
-  // .patch("/:id",authentication, validateOrder,  updateTicket)
   .delete("/:id", authentication, cancelOrderWithId);
 export default ticketRouter;
